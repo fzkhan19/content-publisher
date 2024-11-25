84039ba4057a2dc55bded80e567ac62ad02a18f4
Faiz Khan <faizpathan1717@gmail.com>
2024-11-25 19:15:18 +0530

    feat: content bot created and setup done

---
+++ src/config/credentials.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+
+class Credentials:
+    def __init__(self):
+
+        load_dotenv()
+
+        # X (Twitter) credentials
+        self.TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+        self.TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+        self.TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+        self.TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
+
+        # LinkedIn credentials
+        self.LINKEDIN_ACCESS_TOKEN = os.getenv("LINKEDIN_ACCESS_TOKEN")
+        self.LINKEDIN_USER_ID = os.getenv("LINKEDIN_USER_ID")
+
+        # Bluesky credentials
+        self.BLUESKY_HANDLE = os.getenv("BLUESKY_HANDLE")
+        self.BLUESKY_APP_PASSWORD = os.getenv("BLUESKY_APP_PASSWORD")
